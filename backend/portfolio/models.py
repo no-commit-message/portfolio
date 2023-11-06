5fe7ef210b341a30640459f283c8d01b2e7275a3
川原歩夢 <ayumu@kawaharumenoAir.AirPort>
2023-11-06 16:29:06 +0900

    Update Model to use image and skill level

--- backend/portfolio/models.py
+++ backend/portfolio/models.py
@@ -4,6 +4,8 @@ class Post(models.Model):
     title = models.CharField("タイトル", max_length=100)
     content = models.TextField("本文")
     created_at = models.DateTimeField("作成日", auto_now_add=True)
+    skill_level = models.SmallIntegerField("スキルレベル", default=0)
+    image = models.ImageField("画像", upload_to='images/')
 
     def __str__(self):
         return self.title   
