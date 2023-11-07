7662c054e0aee566768afbbf26b745802626313f
川原歩夢 <ayumu@kawaharaayumiyumenoMacBook-Air.local>
2023-11-07 15:21:23 +0900

    Update Model

--- backend/portfolio/models.py
+++ backend/portfolio/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     content = models.TextField("本文")
     created_at = models.DateTimeField("作成日", auto_now_add=True)
     skill_level = models.SmallIntegerField("スキルレベル", default=0)
-    image = models.ImageField("画像", upload_to='images/')
+    image = models.ImageField("画像", upload_to='images/' ,blank=True, null=True)
 
     def __str__(self):
         return self.title   
